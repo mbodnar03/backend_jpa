9ac75a71b4c44b36e3767b18cd49e7af33886614
mbodn <mbodnar033@gmail.com>
2025-04-10 15:34:52 +0200

    Lab2 - zadanie 1

---
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
@@ -0,0 +1,46 @@
+package com.jpacourse.mapper;
+
+import com.jpacourse.dto.AddressTO;
+import com.jpacourse.dto.PatientTO;
+import com.jpacourse.persistance.entity.AddressEntity;
+import com.jpacourse.persistance.entity.PatientEntity;
+
+public final class PatientMapper
+{
+
+    public static PatientTO mapToTo(final PatientEntity patientEntity)
+    {
+        if (patientEntity == null)
+        {
+            return null;
+        }
+        final PatientTO patientTO = new PatientTO();
+        patientTO.setId(patientEntity.getId());
+        patientTO.setFirstName(patientEntity.getFirstName());
+        patientTO.setLastName(patientEntity.getLastName());
+        patientTO.setTelephoneNumber(patientEntity.getTelephoneNumber());
+        patientTO.setEmail(patientEntity.getEmail());
+        patientTO.setPatientNumber(patientEntity.getPatientNumber());
+        patientTO.setDateOfBirth(patientEntity.getDateOfBirth());
+        patientTO.setInsured(patientEntity.getInsured());
+        return patientTO;
+    }
+
+    public static PatientEntity mapToEntity(final PatientTO patientTO)
+    {
+        if(patientTO == null)
+        {
+            return null;
+        }
+        PatientEntity patientEntity = new PatientEntity();
+        patientEntity.setId(patientTO.getId());
+        patientEntity.setFirstName(patientTO.getFirstName());
+        patientEntity.setLastName(patientTO.getLastName());
+        patientEntity.setTelephoneNumber(patientTO.getTelephoneNumber());
+        patientEntity.setEmail(patientTO.getEmail());
+        patientEntity.setPatientNumber(patientTO.getPatientNumber());
+        patientEntity.setDateOfBirth(patientTO.getDateOfBirth());
+        patientEntity.setInsured(patientTO.getInsured());
+        return patientEntity;
+    }
+}
